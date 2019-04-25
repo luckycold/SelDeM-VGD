a30b530941a5764fd76c4fec19681c22e980bfa8
Ken Nguyen <kennn1510@gmail.com>
2019-04-25 13:31:34 -0500

    scrapped the dead stuff

--- SelDeM/SelDeM/SelDeM/DialogueTreeBuilder.cs
+++ SelDeM/SelDeM/SelDeM/DialogueTreeBuilder.cs
@@ -31,54 +31,12 @@ namespace SelDeM
             font = content.Load<SpriteFont>("DialogChoiceFont");
             StreamReader reader = new StreamReader(path);
             int length = 0;
-            while (!reader.EndOfStream)
-            {
-                if (!reader.ReadLine().Contains("*+"))
-                {
-                    reader.ReadLine();
-                    length++;
-                }
-                else
-                    continue;
-            }
             text = new string[length];
-            readFile(path);
 
             DialogBox parent = new DialogBox(this.spriteBatch, this.content, this.graphics, text[0]);
             tree = new DialogTree<DialogBox>(parent);
-            //children = new DialogBox[2];
-            //children[0] = new DialogBox(spriteBatch, content, graphics, text[1]);
-            //children[1] = new DialogBox(spriteBatch, content, graphics, text[2]);
-
             
-
-            //reader.DiscardBufferedData();
-            //int i = 0;
-            //int nodeNum = 0;
-            //bool hasMore = false;
-            //reader.ReadLine(); //to read the parent text
-            //while(!reader.EndOfStream)
-            //{
-            //    string temp = reader.ReadLine();
-            //    i++;
-            //    if (temp.Equals("*+2"))
-            //    {
-            //        nodeNum++;
-            //        tree[nodeNum - 1].AddChild(new DialogBox(spriteBatch, content, graphics, text[i + 1])); //nodeNum-1 to go to parent and i+1 to look forward one in array
-            //        hasMore = true;
-            //        //Need to keep in mind when the second child of this gen gets made
-            //    }
-            //    else if (temp.Equals("*+E"))
-            //    {
-            //        tree[nodeNum - 1].AddChild(new DialogBox(spriteBatch, content, graphics, text[i - 1]));
-            //    }
-            //    else
-            //        continue;
-            //}
-
-
-            //tree.AddChildren(children);
-            //tree[2].addChildren();
+            
         }
 
         public DialogTree<DialogBox> getDialogBox(int i) //The way to use this is to first have an object of DialogueTreeBuilder, then: [objectName].getDialogBox([nodeNumber]).[DialogBoxMethods];
@@ -86,24 +44,4 @@ namespace SelDeM
             return tree[i];
         }
 
-        private void readFile(string path)
-        {
-            StreamReader reader = new StreamReader(path);
-            int i = 0;
-            while (!reader.EndOfStream)
-            {
-                //string temp = reader.ReadLine();
-                //if (!temp.Contains("*+"))
-                //{
-                //    text[i] = temp;
-                //    i++;
-                //}
-                //else
-                //    continue;
-                string temp = reader.ReadLine();
-                text[i] = temp;
-                i++;
-            }
-        }
-    }
 }
